f9a13e6b2a07a0517e793a7a7dde4c3e631e7c80
Travis Fischer <fisch0920@gmail.com>
2025-03-08 20:46:26 +0800

    🛁

--- src/app/o/[id]/image/route.tsx
+++ src/app/o/[id]/image/route.tsx
@@ -1,5 +1,11 @@
 import { generateWellnessFactImageResponse } from '@/lib/generate-wellness-fact-image-response'
 
+// Image metadata
+export const size = {
+  width: 2400,
+  height: 1200
+}
+export const contentType = 'image/png'
 export const dynamic = 'force-static'
 
 export async function GET(
